ee82d6a4157a4408d05cf90ce45e3a69d5a88906
ljyo2o9 <yggosxoljy@gmail.com>
2023-09-11 16:50:43 +0900

    Quick 정렬

--- C-Practice/C-Practice/main.c
+++ C-Practice/C-Practice/main.c
@@ -1,44 +1,65 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include<stdlib.h>
 #include <time.h>
 
-void shellSort(int arr[], int n) {
-    int k, i, j, temp; //k = gap n = 10;
+#define MAX_SIZE 10
 
-    for (k = n / 2; k > 0; k /= 2) {
-        if(k % 2 == 0) k++;
-        for (i = k; i < n; i++) {
+int arr[MAX_SIZE];
+
+// 배열을 분할하는 함수
+int partition(int arr[], int low, int high) {
+    int temp;
+    
+    // 피벗을 배열의 가장 오른쪽 요소로 선택
+    int pivot = arr[high];
+    int i = (low - 1);  // 작은 요소들의 인덱스
+
+    for (int j = low; j <= high - 1; j++) {
+        // 현재 요소가 피벗보다 작으면, 작은 요소들을 한 쪽으로 모음
+        if (arr[j] < pivot) {
+            i++;
             temp = arr[i];
-            for (j = i; j >= k && arr[j - k] > temp; j -= k) {
-                arr[j] = arr[j - k];
-            }
+            arr[i] = arr[j];
             arr[j] = temp;
         }
+    }
+    
+    temp = arr[i+1];
+    arr[i+1] = arr[high];
+    arr[high] = temp;
+    
+    return (i + 1);
+}
+
+// 퀵 정렬 함수
+void quickSort(int arr[], int low, int high) {
+    if (low < high) {
+        // 배열을 분할하고 피벗의 위치를 얻음
+        int pi = partition(arr, low, high);
+
+        // 분할된 두 부분을 각각 정렬
+        quickSort(arr, low, pi - 1); //왼쪽 퀵 정렬
+        quickSort(arr, pi + 1, high); // 오른쪽 퀵정렬
     }
 }
 
 int main(void) {
-    int arr[10];
-    int n = 10;
-
+    int n = MAX_SIZE;
     srand(time(NULL));
-    for (int i = 0; i < n; i++) {
-        arr[i] = rand() % 100;
-    }
+    for (int i = 0; i < n; i++)
+            arr[i] = rand() % 100;
 
-    printf("정렬 전 : ");
-    for (int i = 0; i < n; i++) {
-        printf("%d ", arr[i]);
-    }
-    printf("\n");
-
-    shellSort(arr, n);
-
-    printf("정렬 후 : ");
+    printf("정렬 전 배열: ");
     for (int i = 0; i < n; i++) {
         printf("%d ", arr[i]);
     }
-    printf("\n");
+
+    quickSort(arr, 0, n - 1);
+
+    printf("\n정렬 후 배열: ");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
 
     return 0;
 }
