623a9c201db1501a52095119adabd35aedc3940f
ljyo2o9 <yggosxoljy@gmail.com>
2023-09-23 13:51:40 +0900

    프로그래머스 유한소수 판별하기

--- C-Practice/C-Practice/main.c
+++ C-Practice/C-Practice/main.c
@@ -1,30 +1,61 @@
-//전국 대회 선발 고사
+// 유한 소수 판별하기
 #include <stdio.h>
 #include <stdbool.h>
 #include <stdlib.h>
 
-// rank_len은 배열 rank의 길이입니다.
-// attendance_len은 배열 attendance의 길이입니다.
-int solution(int rank[], size_t rank_len, bool attendance[], size_t attendance_len) {
-    int answer = 0, suu = 0, sum = 0;
-    int arr[5] = {0,};
-    for(int i=0 ; i < rank_len; i++){
-        if(suu == 3) break;
-        for(int j=0; j < rank_len; j++){
-            if(rank[j] == i+1){
-                if(attendance[j] == true){
-                    arr[suu] = j;
-                    suu++;
-                }
+int sosu(int d){  //7 // 2 ~ 6 //
+    int suu = 0;
+    if(d == 2) return 0;
+    if(d == 5) return 0;
+    for(int i=2; i<d; i++){
+        if(d % i == 0){
+            if(i % 2 == 0){
+                suu++;
+            } else if (i % 5 == 0){
+                suu++;
+            } else {
+                return 1;
             }
         }
     }
-    for(int i=0; i<3; i++)
-        printf("%d\n", arr[i]);
     
-    sum = 10000 * arr[0] + 100 * arr[1] + arr[2];
+    if(suu > 0){
+        return 0;
+    } else {
+        return 1;
+    }
+}
+
+int solution(int a, int b) {
+    int answer = 0;
     
-    answer = sum;
+    if(b % a == 0){
+        b = b / a;
+        a = 1;
+    }
+ 
+    for(int i = (a/2) + 1; i>1; i--){
+        if(a == 1) break;
+        else if(b % i == 0 && a % i == 0){
+            a = a/i;
+            b = b/i;
+            
+            i++;
+        }
+    }
     
+    printf("--%d--%d--", a, b);
+    
+    if(a == b || a % b == 0) {
+        answer = 1;
+        return answer;
+    }
+    if(sosu(b) == 1){
+        answer = 2;
+    } else {
+        answer = 1;
+    }
+    printf("---%d---", answer);
+            
     return answer;
 }
