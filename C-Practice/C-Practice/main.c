89915ea81720d7f27e737689a62405569d172697
ljyo2o9 <yggosxoljy@gmail.com>
2023-09-23 13:50:28 +0900

    프로그래머스 전국 대회 선발 고사

--- C-Practice/C-Practice/main.c
+++ C-Practice/C-Practice/main.c
@@ -1,97 +1,30 @@
-#include<stdio.h>
-#include<string.h>
-#define MAX_STACK_SIZE 100
+//전국 대회 선발 고사
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
 
-struct stacks {
-    char stack[MAX_STACK_SIZE];
-    int top;
-};
-
-void init_stack(struct stacks* s) {
-    s->top = -1;
-}
-
-//스택과 관련된 함수 모두 작성
-int is_full(struct stacks* s){
-    return s->top == MAX_STACK_SIZE - 1;
-}
-
-int is_empty(struct stacks* s){
-    return s->top == -1;
-}
-
-void push(struct stacks* s, char c){
-    if(is_full(s)) return;
-    s->stack[++(s->top)] = c;
-}
-
-char pop(struct stacks* s){
-    if(is_empty(s)) return 0;
-    return s->stack[(s->top)--];
-}
-
-char peek(struct stacks* s) {
-    if (is_empty(s)){
-        return 0;
-    }
-    else return s->stack[s->top];
-}
-
-//연산자 우선순위 결정하는 함수
-int rank(char c) {
-    if (c == '(' || c == ')')
-        return 0;
-    else if (c == '+' || c == '-')
-        return 1;
-    else if (c == '*' || c == '/')
-        return 2;
-    else return -1;
-}
-
-//중위 -> 후위
-void infix_to_postfix(char* s) {
-    struct stacks st;
-    char ch, c;
-    int length = strlen(s);
-    init_stack(&st);
-    for (int i = 0; i < length; i++) {
-        ch = s[i];
-        switch (ch){
-            case '+':
-            case '-':
-            case '*':
-            case '/':
-                while(rank(peek(&st)) >= rank(ch)){
-                    c = pop(&st);
-                    printf("%c", c);
+// rank_len은 배열 rank의 길이입니다.
+// attendance_len은 배열 attendance의 길이입니다.
+int solution(int rank[], size_t rank_len, bool attendance[], size_t attendance_len) {
+    int answer = 0, suu = 0, sum = 0;
+    int arr[5] = {0,};
+    for(int i=0 ; i < rank_len; i++){
+        if(suu == 3) break;
+        for(int j=0; j < rank_len; j++){
+            if(rank[j] == i+1){
+                if(attendance[j] == true){
+                    arr[suu] = j;
+                    suu++;
                 }
-                push(&st, ch);
-                break;
-            case '(':
-                push(&st, ch);
-                break;
-            case ')':
-                c = pop(&st);
-                while(c != '('){
-                    printf("%c", c);
-                    c = pop(&st);
-                }
-                break;
-            default:
-                printf("%c", ch);
+            }
         }
     }
-    //스택에 남아있는 것들 모두 출력
-    while(!is_empty(&st)){
-        printf("%c", pop(&st));
-    }
-}
-
-int main(void) {
-    char* s = "(2+3)*4+9";
-    printf("중위표기수식 %s\n", s);
-    printf("후위표기수식 ");
-    infix_to_postfix(s);
-    printf("\n");
-    return 0;
+    for(int i=0; i<3; i++)
+        printf("%d\n", arr[i]);
+    
+    sum = 10000 * arr[0] + 100 * arr[1] + arr[2];
+    
+    answer = sum;
+    
+    return answer;
 }
