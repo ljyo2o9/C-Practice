6aac8b43476a6278f83266702787c27384960276
ljyo2o9 <yggosxoljy@gmail.com>
2023-09-26 15:07:02 +0900

    스택[후위 표기법 계산하기]

--- C-Practice/C-Practice/main.c
+++ C-Practice/C-Practice/main.c
@@ -1,20 +1,96 @@
-#include <stdio.h>
+#include<stdio.h>
+#include<string.h>
+#define MAX_SIZE 100
 
-int main(void){
-    int a, b, c, avg = 0;
-    scanf("%d %d %d", &a, &b, &c);
-    
-    if(a > b){
-        if(a < c) avg = a;
-        else if (c > b) avg = c;
-        else avg = b;
+struct stacks {
+    int stack[MAX_SIZE];
+    int top;
+};
+
+// 스택 초기화
+void init_stack(struct stacks *st) {
+    st->top = -1;
+}
+
+// 스택이 비어있는지 확인
+int is_empty(struct stacks *st) {
+    return (st->top == -1);
+}
+
+// 스택이 꽉 찼는지 확인
+int is_full(struct stacks *st) {
+    return (st->top == MAX_SIZE - 1);
+}
+
+// 스택에 원소 삽입
+void push(struct stacks *st, int item) {
+    if (is_full(st)) {
+        printf("Stack is full.\n");
     } else {
-        if(b < c) avg = b;
-        else if (c > a) avg = c;
-        else avg = a;
+        st->stack[++(st->top)] = item;
     }
-    
-    printf("%d\n", avg);
-    
+}
+
+// 스택에서 원소 제거 및 반환
+int pop(struct stacks *st) {
+    if (is_empty(st)) {
+        printf("Stack is empty.\n");
+        return -1;
+    } else {
+        return st->stack[(st->top)--];
+    }
+}
+
+// 후위표기식 계산
+int calc_postfix(char exp[]) {
+    struct stacks st;
+    int first, second, value;
+    long long int len = strlen(exp);
+    char c;
+
+    init_stack(&st);
+
+    for (int i = 0; i < len; i++) {
+        c = exp[i];
+
+        if(c >= '0' && c <= '9') push(&st,c-'0');
+        else{
+            second = pop(&st);
+            first = pop(&st) + 0.0;
+
+            switch(c){
+                case '+':
+                    value = first + second;
+                    break;
+
+                case '-':
+                    value = first - second;
+                    break;
+
+                case '*':
+                    value = first * second;
+                    break;
+
+                case '/':
+                    value = first / second;
+                    break;
+                default:
+                    value = -999999;
+                    return printf("식이 잘못되었습니다\n");
+            }
+
+            push(&st,value);
+        }
+    }
+
+    return pop(&st); // 마지막 연산 결과 반환.
+
+}
+
+
+int main(void) {
+    printf("후위표기식은 : 43+52-*2+\n");
+    printf("결과값은 %d\n", calc_postfix("43+52-*2+"));
+
     return 0;
 }
