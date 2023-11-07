8cba600113f85550611854ae9e9e694bb3233b76
ljyo2o9 <yggosxoljy@gmail.com>
2023-11-07 15:22:11 +0900

    원형 연결 리스트 완성

--- C-Practice/C-Practice/main.c
+++ C-Practice/C-Practice/main.c
@@ -1,91 +1,157 @@
+//#include "stdio.h"
+//#include "stdlib.h"
+//
+//struct Listnode {
+//    int data;
+//    struct Listnode* link;
+//};
+//
+//struct Listnode* insert_first(struct Listnode* head, int value) {
+//    // p를 먼저 만들자.
+//    struct Listnode* p = (struct Listnode*)malloc(sizeof(struct Listnode));
+//    // p의 data 필드에 값을 넣는다.
+//    p -> data = value;
+//    // p의 link값에는 head를 향하게 두고
+//    p -> link = head;
+//    // head는 p를 향하게 둔다.
+//    head = p;
+//
+//    return head;
+//}
+//
+//struct Listnode* insert(struct Listnode* head, struct Listnode* pre, int value) {
+//    // p를 먼저 만들자
+//    struct Listnode* p = (struct Listnode*)malloc(sizeof(struct Listnode));
+//    // p의 data필드에 값을 넣는다
+//    p -> data = value;
+//    // p의 link값에는 pre다음의 들어갈 link를 넣는다
+//    p -> link = pre->link;
+//    // pre의 link필드는 p에 넣어준다
+//    pre->link = p;
+//    return head;
+//}
+//
+//struct Listnode* delete_first(struct Listnode* head) {
+//    // 삭제할 너도를 저장할 removed를 생성
+//    struct Listnode* removed = head;
+//    // head의 link위치를 그 다음 node로 바꾸어준다
+//    head = head -> link;
+//    // free를 해준다
+//    free(removed);
+//
+//    return head;
+//}
+//
+//struct Listnode* delete(struct Listnode* head, struct Listnode* pre) {
+//    // node removed
+//    struct Listnode* removed = pre->link;
+//
+//    pre->link = pre->link->link;
+//    free(removed);
+//
+//    return head;
+//}
+//
+//
+//void print_list(struct Listnode* head) {
+//    //struct Listnode* p = head;
+//    while (head != NULL) {
+//        printf("%d -> ", head->data);
+//        head = head->link;
+//    }
+//    printf("NULL\n");
+//}
+//
+//int main(void) {
+//    struct Listnode* head = NULL;
+//    head = (struct Listnode*)malloc(sizeof(struct Listnode));
+//    head->data = 1;
+//    head->link = NULL;
+//
+//    head = insert_first(head, 2);
+//    head = insert_first(head, 3);
+//    head = insert_first(head, 4);
+//    head = insert_first(head, 5);
+//
+//    print_list(head);
+//    head = delete(head, head->link->link);
+//
+//    print_list(head);
+//
+//    return 0;
+//}
+
 #include "stdio.h"
 #include "stdlib.h"
 
-struct Listnode {
+struct Node {
     int data;
-    struct Listnode* link;
+    struct Node* link;
 };
 
-struct Listnode* insert_first(struct Listnode* head, int value) {
-    // p를 먼저 만들자.
-    struct Listnode* p = (struct Listnode*)malloc(sizeof(struct Listnode));
-    // p의 data 필드에 값을 넣는다.
-    p -> data = value;
-    // p의 link값에는 head를 향하게 두고
-    p -> link = head;
-    // head는 p를 향하게 둔다.
-    head = p;
+struct Node* head = NULL;
+
+void insert_first(int value) {
+    struct Node* node = (struct Node*)malloc(sizeof(struct Node));
+    node -> data = value;
     
-    return head;
-}
-
-struct Listnode* insert(struct Listnode* head, struct Listnode* pre, int value) {
-    // p를 먼저 만들자
-    struct Listnode* p = (struct Listnode*)malloc(sizeof(struct Listnode));
-    // p의 data필드에 값을 넣는다
-    p -> data = value;
-    // p의 link값에는 pre다음의 들어갈 link를 넣는다
-    p -> link = pre->link;
-    // pre의 link필드는 p에 넣어준다
-    pre->link = p;
-    return head;
-}
-
-struct Listnode* delete_first(struct Listnode* head) {
-    // 삭제할 너도를 저장할 removed를 생성
-    struct Listnode* removed = head;
-    // head의 link위치를 그 다음 node로 바꾸어준다
-    head = head -> link;
-    // free를 해준다
-    free(removed);
-    
-    return head;
-}
-
-struct Listnode* delete(struct Listnode* head, struct Listnode* pre) {
-    // node removed
-    struct Listnode* removed = pre->link;
-    
-    pre->link = pre->link->link;
-    free(removed);
-    
-    return head;
-}
-
-
-void print_list(struct Listnode* head) {
-    //struct Listnode* p = head;
-    while (head != NULL) {
-        printf("%d -> ", head->data);
-        head = head->link;
+    if(head == NULL) {
+        head = node;
+        node->link = head;
     }
-    printf("NULL\n");
+    else {
+        node->link = head->link;
+        head->link = node;
+    }
+}
+
+void insert_last(int value) {
+    struct Node* node = (struct Node*)malloc(sizeof(struct Node));
+    node -> data = value;
+    
+    if (head == NULL){
+        head = node;
+        node->link = head;
+    } else {
+        node->link = head->link;
+        head->link = node;
+        head = node;
+    }
+}
+
+void delete_first(void) {
+    struct Node* removed = head->link;
+    head->link = removed -> link;
+    
+    free(removed);
+}
+
+void print_list(void) {
+    struct Node* node = head->link;
+    
+    if(head == NULL) return;
+    do {
+        printf("%d -> ", node->data);
+        node = node->link;
+    } while(node != head->link);
+    
+    printf("\n");
 }
 
 int main(void) {
-    struct Listnode* head = NULL;
-    head = (struct Listnode*)malloc(sizeof(struct Listnode));
-    head->data = 2;
-    head->link = NULL;
+    insert_first(10);
+    insert_first(20);
+    insert_first(30);
     
-    head = insert_first(head, 1);
-    head = insert_first(head, 10);
-    head = insert_first(head, 10);
-    head = insert_first(head, 5);
-    head = insert_first(head, 5);
-    head = insert(head, head->link, 8);
+    print_list();
     
-    head = delete_first(head);
-    head = delete_first(head);
-    head = delete_first(head);
+    insert_last(40);
     
-    print_list(head);
+    print_list();
     
-    head = insert_first(head, 5);
-    head = insert_first(head, 5);
-    head = delete(head, head);
-    head = delete(head, head);
+    delete_first();
     
-    print_list(head);
+    print_list();
+    
     return 0;
 }
