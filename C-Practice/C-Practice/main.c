819201dd4c59906edf2f860ba72aa92046d41921
ljyo2o9 <yggosxoljy@gmail.com>
2023-11-28 15:06:46 +0900

    이진 탐색 트리(삭제)

--- C-Practice/C-Practice/main.c
+++ C-Practice/C-Practice/main.c
@@ -6,6 +6,14 @@ struct Treenode {
     struct Treenode *left, *right;
 };
 
+void ino(struct Treenode* node){
+    if(node != NULL){
+        ino(node->left);
+        printf("%d ", node->data);
+        ino(node->right);
+    }
+}
+
 struct Treenode* search(struct Treenode* node, int key) {
     if (node == NULL) return NULL;
     if (key == node->data) return node;
@@ -23,13 +31,44 @@ struct Treenode* new_node(int key) {
     return new;
 }
 
+struct Treenode* min_value(struct Treenode* node) {
+    struct Treenode* current = node;
+    while (current->left != NULL) {
+        current = current->left;
+    }
+
+    return current;
+}
+
+struct Treenode* delete(struct Treenode* node, int key) {
+    if (node == NULL) return node;
+
+    if (node->data > key) node->left = delete(node->left, key);
+    else if (node->data < key) node->right = delete(node->right, key);
+    else {
+        if (node->left == NULL) {
+            struct Treenode* temp = node->right;
+            free(node);
+            return temp;
+        }
+        else if (node->right == NULL) {
+            struct Treenode* temp = node->left;
+            free(node);
+            return temp;
+        }
+        struct Treenode* temp = min_value(node->right);
+        node->data = temp->data;
+        node->right = delete(node->right, temp->data);
+    }
+
+    return node;
+}
+
 struct Treenode* insert(struct Treenode* node, int key) {
     if (node == NULL) return new_node(key);
 
-    if (node->data > key)
-        node->left = insert(node->left, key);
-    else if (node->data < key)
-        node->right = insert(node->right, key);
+    if (node->data > key) node->left = insert(node->left, key);
+    else if (node->data < key) node->right = insert(node->right, key);
 
     return node;
 }
@@ -39,11 +78,19 @@ int main(void){
     int n;
     
     while(1){
-        printf("삽입(0을 입력하면 원하는 값을 찾음) : ");
+        printf("삽입 : ");
         scanf("%d", &n);
         if(n == 0) break;
         
         tree = insert(tree, n);
+    }
+    
+    while (1) {
+        printf("삭제 : ");
+        scanf("%d", &n);
+        if(n == 0) break;
+        
+        tree = delete(tree, n);
     }
     
     while(1){
@@ -57,6 +104,10 @@ int main(void){
             printf("%d(는)은 없음\n", n);
         }
     }
+    
+    printf("잘 들어갔나?? - ");
+    ino(tree);
+    printf("\n");
 
     return 0;
 }
