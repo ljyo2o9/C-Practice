a2612f0c78282c4cffc4c244a0563b02a1158615
ljyo2o9 <yggosxoljy@gmail.com>
2024-01-04 10:00:15 +0900

    그래프 인접 리스트

--- C-Practice/C-Practice/main.c
+++ C-Practice/C-Practice/main.c
@@ -1,21 +1,22 @@
 #include<stdio.h>
 #include<stdlib.h>
-
 #define MAX_VERTICES 50
+
+struct Graphnode {
+    int vertex;
+    struct Graphnode* link;
+};
 
 struct Graph {
     int n; //정점의 수
-    int adj_mat[MAX_VERTICES][MAX_VERTICES];
+    struct Graphnode* adj_list[MAX_VERTICES];
 };
 
 //초기화
 void init(struct Graph* g) {
     g->n = 0;
-    for (int r = 0; r < MAX_VERTICES; r++) {
-        for (int c = 0; c < MAX_VERTICES; c++) {
-            g->adj_mat[r][c] = 0;
-        }
-    }
+    for (int v = 0; v < MAX_VERTICES; v++)
+        g->adj_list[v] = NULL;
 }
 
 //정점 삽입
@@ -27,21 +28,33 @@ void insert_vertex(struct Graph* g, int v) {
     g->n++;
 }
 
-//간선 삽입
-void insert_edge(struct Graph* g, int start, int end) {
-    if (g->n - 1 < start || g->n - 1 < end || start == end || start < 0 || end < 0) {//4. 그래프 정점 오류 조건은?
+//간선 삽입 v를 u의 인접 리스트에 삽입한다. (연결 리스트 맨 앞에 삽입)
+void insert_edge(struct Graph* g, int u, int v) {
+    struct Graphnode* node;
+    struct Graphnode* node_reverse;
+    if (u >= g->n || v >= g->n) {
         printf("그래프 : 정점 번호 오류\n");
         return;
     }
-    g->adj_mat[start][end] = 1;
-    g->adj_mat[end][start] = 1;
+    node = (struct Graphnode*)malloc(sizeof(struct Graphnode));
+    node->vertex = v;
+    node->link = g->adj_list[u];
+    g->adj_list[u] = node;
+    
+    node_reverse = (struct Graphnode*)malloc(sizeof(struct Graphnode));
+    node_reverse->vertex = u;
+    node_reverse->link = g->adj_list[v];
+    g->adj_list[v] = node_reverse;
 }
 
-//인접 행렬 출력
+//인접 리스트 출력
 void print(struct Graph* g) {
-    for(int i=0; i<g->n; i++){
-        for(int j=0; j<g->n; j++){
-            printf("%d ", g->adj_mat[i][j]);
+    for (int i = 0; i < g->n; i++) {
+        struct Graphnode* p = g->adj_list[i];
+        printf("정점 %d의 인접 리스트", i);
+        while (p != NULL) {
+            printf(" -> %d", p->vertex);
+            p = p->link;
         }
         printf("\n");
     }
@@ -50,22 +63,19 @@ void print(struct Graph* g) {
 int main(void) {
     struct Graph* g;
     g = (struct Graph*)malloc(sizeof(struct Graph));
-
     init(g);
 
-    for (int i = 0; i < 4; i++) {
+    for (int i = 0; i < 4; i++)
         insert_vertex(g, i);
-    }
 
     insert_edge(g, 0, 1);
-    insert_edge(g, 0, 2);
     insert_edge(g, 0, 3);
+    insert_edge(g, 0, 2);
     insert_edge(g, 1, 2);
     insert_edge(g, 2, 3);
 
     print(g);
 
     free(g);
-
     return 0;
 }
