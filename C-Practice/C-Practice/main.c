b7b7d869f801cf461ab9fb5c8837b7b58509f7ae
ljyo2o9 <yggosxoljy@gmail.com>
2024-01-04 10:30:53 +0900

    깊이 우선 탐색 구현(인접 리스트)

--- C-Practice/C-Practice/main.c
+++ C-Practice/C-Practice/main.c
@@ -2,52 +2,64 @@
 #include<stdlib.h>
 #define MAX_VERTICES 50
 
-int visited[MAX_VERTICES]; //방문한 정점을 저장하는 배열
+int visited[MAX_VERTICES]; //정점을 방문했는지를 저장하는 배열
 
-//인접행렬 코드 그대로!
+//인접리스트 코드 그대로!
 
-struct Graph {
-    int n; //정점의 개수
-    int adj_mat[MAX_VERTICES][MAX_VERTICES];
+struct Graphnode {
+    int vertex;
+    struct Graphnode* link;
 };
 
-//그래프 초기화
+struct Graph {
+    int n; //정점의 수
+    struct Graphnode* adj_list[MAX_VERTICES];
+};
+
+//초기화
 void init(struct Graph* g){
     g->n = 0;
-    for(int i=0; i<MAX_VERTICES; i++)
-        for(int j=0; j<MAX_VERTICES; j++)
-            g->adj_mat[i][j] = 0;
+    for(int i=0; i<MAX_VERTICES; i++){
+        g->adj_list[i] = NULL;
+    }
 }
 
-//정점 삽입 연산
+//정점 삽입
 void insert_vertex(struct Graph* g, int v){
-    if ((g->n + 1) > MAX_VERTICES) {
-        printf("그래프 : 정점 개수 초과\n");
+    if(v > MAX_VERTICES){
+        printf("error\n");
         return;
     }
     g->n++;
 }
 
-//간선 삽입 연산
-void insert_edge(struct Graph* g, int start, int end){
-    if (g->n - 1 < start || g->n - 1 < end || start == end || start < 0 || end < 0) {
+//간선 삽입 v를 u의 인접 리스트에 삽입한다. (연결 리스트 맨 앞에 삽입)
+void insert_edge(struct Graph* g, int u, int v) {
+    struct Graphnode* node;
+    struct Graphnode* node_reverse;
+    if (u >= g->n || v >= g->n) {
         printf("그래프 : 정점 번호 오류\n");
         return;
     }
-    g->adj_mat[start][end] = 1;
-    g->adj_mat[end][start] = 1;
+    node = (struct Graphnode*)malloc(sizeof(struct Graphnode));
+    node->vertex = v;
+    node->link = g->adj_list[u];
+    g->adj_list[u] = node;
+    
+    node_reverse = (struct Graphnode*)malloc(sizeof(struct Graphnode));
+    node_reverse->vertex = u;
+    node_reverse->link = g->adj_list[v];
+    g->adj_list[v] = node_reverse;
 }
 
-//깊이 우선 탐색
+//dfs 출력
 void dfs(struct Graph* g, int v) {
-    int w;
+    struct Graphnode* p;
     visited[v] = 1;
     printf("정점 %d -> ", v);
-    for (w = 0; w < g->n; w++) {
-//        if (visited[w] != 1 && ) {dfs(g, w);}
-        if(visited[w] != 1 && g->adj_mat[v][w] == 1){
-            dfs(g, w);
-        }
+    for (p = g->adj_list[v]; p; p = p->link) {
+        if (visited[p->vertex] != 1)
+            dfs(g, p->vertex);
     }
 }
 
@@ -65,7 +77,6 @@ int main(void) {
     insert_edge(g, 1, 2);
     insert_edge(g, 2, 3);
     
-    printf("깊이 우선 탐색\n");
     dfs(g, 0);
     free(g);
     return 0;
